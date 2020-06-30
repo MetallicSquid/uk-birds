8f8af46ad35feefecd17f96d11eca393eae75b09
MetallicSquid <guillame.Macneil@ccoex.com>
2020-06-30 13:12:14 +0100

    Commit for better model naming

--- model.py
+++ model.py
@@ -3,8 +3,8 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 
-# Build the model
-def build_model():
+# Build the basic model
+def build_basic():
     print("Building basic model...")
     # Load the images and labels
     train_images = np.load('train_images.npy', allow_pickle=True)
@@ -32,6 +32,45 @@ def build_model():
     print("Test Loss:", test_loss)
 
     # Save the model
-    model.save('nn_model/model')
-
+    model.save('nn_model/basic')
     print("\n...Basic model built.")
+
+# Build the convolutional model
+def build_conv():
+    print("Building convolutional model...")
+    # Load the images and labels
+    train_images = np.load('train_images.npy', allow_pickle=True)
+    test_images = np.load('test_images.npy', allow_pickle=True)
+    train_labels = np.load('train_labels.npy', allow_pickle=True)
+    test_labels = np.load('test_labels.npy', allow_pickle=True)
+
+    # Reformat data for CNN
+    row_num = train_images[0].shape[0]
+    col_num = train_images[0].shape[1]
+    train_images = train_images.reshape(train_images.shape[0], row_num, col_num, 1)
+    test_images = test_images.reshape(test_images.shape[0], row_num, col_num, 1)
+
+    # Form, compile and feed the model
+    model = keras.Sequential()
+    model.add(keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(row_num, col_num, 1)))
+    model.add(keras.layers.MaxPooling2D((2, 2)))
+    model.add(keras.layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(keras.layers.MaxPooling2D((2, 2)))
+    model.add(keras.layers.Conv2D(128, (3, 3), activation='relu'))
+
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(256, activation='relu'))
+    model.add(keras.layers.Dense(2))
+
+    model.compile(optimizer='adam',
+            loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+            metrics=['accuracy'])
+
+    model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
+
+    # Save the model
+    model.save('nn_model/convolutional')
+    print('...Convolutional model built.')
+
+
+
--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from dataset_prep import split_and_save_datasets
-from model import build_model
+from model import build_basic, build_conv
 
 print("Setting up the model, this will take a few minutes...\n")
-split_and_save_datasets()
-build_model()
+#split_and_save_datasets()
+#build_basic()
+build_conv()
 print("\n...Model has been set up.")
